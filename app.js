703f4d1c0159b0f48e98252aa3fc13f14678bc83
Nazar123456789123123 <131575164+Nazar123456789123123@users.noreply.github.com>
2026-01-06 19:34:52 +0200

    Change Google login to use signInWithPopup
    Updated login button event to use signInWithPopup instead of signInWithRedirect for Google authentication.

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
 const loginScreen = document.getElementById("login-screen");
 const programScreen = document.getElementById("program-screen");
 const loginBtn = document.getElementById("googleLoginBtn");
-loginBtn.addEventListener("click", () => {
+
+loginBtn.addEventListener("click", async () => {
   const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithRedirect(provider);
-});
 
-firebase.auth().getRedirectResult()
-  .then((result) => {
-    if (result.user) {
-      console.log("Успішний вхід:", result.user.email);
-    }
-  })
-  .catch((error) => {
-    console.error("Redirect error:", error);
-  });
-
-firebase.auth().onAuthStateChanged(user => {
-  if (user) {
-    loginScreen.classList.add("hidden");
-    programScreen.classList.remove("hidden");
+  try {
+    await firebase.auth().signInWithPopup(provider);
+  } catch (error) {
+    alert("Помилка входу");
+    console.error(error);
   }
 });
 
